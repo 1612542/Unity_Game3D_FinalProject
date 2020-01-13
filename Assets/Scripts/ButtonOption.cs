f61e367ef4209cb8f9e89d9348dd1ecfa3d5fec9
nhutrg1998 <nhutrg1998@gmail.com>
2020-01-14 01:04:31 +0700

    14-01-2019

---
+++ Assets/Scripts/ButtonOption.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOption : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Track01()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Track02()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Guide()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
