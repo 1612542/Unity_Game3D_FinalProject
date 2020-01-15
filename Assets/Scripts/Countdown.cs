6a352b97ea2cc106badae74a6c08ad753f9ec961
hoangphuhung1998@gmail.com <hoangphuhung1998@gmail.com>
2020-01-15 10:58:48 +0700

    update UI, countdown

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+	public GameObject countBox;
+	float ms;
+	int count;
+    // Start is called before the first frame update 
+    void Start()
+    {
+        ms =0;
+	count =4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ms+=Time.deltaTime*10;
+	transform.Rotate(new Vector3(2.8f,0f,0f));
+	if (ms>=10){
+		count-=1;
+		ms=0;
+		transform.rotation = Quaternion.Euler(0, 0, 0);
+		gameObject.GetComponent<Text>().text=""+count.ToString();
+	}
+	if (count <=-1 ){
+		Destroy(gameObject);
+	}
+	if (count <=0 ){
+		gameObject.GetComponent<Text>().text="GO!";
+	}
+		
+
+    }
+}
