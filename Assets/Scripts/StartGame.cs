5c5b8846a713fd7b446382ef391cf24ff077cbc9
hoangphuhung1998@gmail.com <hoangphuhung1998@gmail.com>
2020-01-15 11:09:24 +0700

    refactor

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+	public GameObject panel;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(5);
+	panel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
